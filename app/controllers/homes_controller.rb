caf836fe2ac4b80b0becba4bc5fea084c57d62c3
rnovick13 <rnovick13@gmail.com>
2019-01-04 09:24:32 -0500

    skateboard yay

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,7 +1,3 @@
 class HomesController < ApplicationController
 
-  def index
-    # @home = Home.find(params[:id])
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+    get "homes/index"
     root 'homes#index'
     resources :meal_plans, only: [:index, :show, :new, :create]
     resources :meal_restrictions, only: [:index, :show, :new, :create]
     resources :meals, only: [:index, :show, :new, :create, :home]
     resources :mp_meals, only: [:index, :new, :create]
     resources :restrictions, only: [:index, :show, :new, :create]
-    # resources :homes, only: [:index]
 end
